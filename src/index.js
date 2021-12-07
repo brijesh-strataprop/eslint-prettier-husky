2c946f6ac181cf8526e3ecf2ca5505cf3db822ec
Brijesh <brijeshk@strataprop.com>
2021-12-07 14:28:29 +0530

    added codepush feature & staging key

--- src/index.js
+++ src/index.js
@@ -3,16 +3,38 @@ import {View} from 'react-native';
 import 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/es/integration/react';
+import SplashScreen from 'react-native-splash-screen';
+import codePush from 'react-native-code-push';
 import configureStore from './config/configure-store';
 import Navigator from './config/navigator';
-import SplashScreen from 'react-native-splash-screen';
+
+const codePushOptions = {checkFrequency: codePush.CheckFrequency.MANUAL};
 
 const {store, persistor} = configureStore();
 
 const Src = () => {
+  const handleCodePush = () => {
+    codePush
+      .checkForUpdate()
+      .then(update => {
+        if (update) {
+          codePush.sync({
+            updateDialog: true,
+            installMode: codePush.InstallMode.IMMEDIATE,
+            mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+          });
+        }
+      })
+      .catch(error => {
+        // report to error log server
+        console.log(`Error: app update - ${error.message}`);
+      });
+  };
+
   React.useEffect(() => {
     // Hide the splash screen
     SplashScreen.hide();
+    handleCodePush();
   }, []);
 
   return (
@@ -26,4 +48,4 @@ const Src = () => {
   );
 };
 
-export default Src;
+export default codePush(codePushOptions)(Src);
